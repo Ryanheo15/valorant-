870ac5d0c2b70c23f750b898e45b3073031d0777
Adory Vo <adoryvo@gmail.com>
2020-08-11 07:04:09 -0700

    styling

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
+const favicon = require('serve-favicon');
+const path = require('path')
 
 const con = mysql.createConnection({
     host: 'localhost',
@@ -23,9 +25,18 @@ function encrypt(input) {
 
 app.use(express.static(__dirname + '/public')); // url path begins at /public
 app.use(bodyParser.urlencoded({extended: false})); // parse application/x-www-form-urlencoded
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 app.post('/signup', function (req, res) {
+    con.query(`SELECT * FROM accounts WHERE email = '${req.body.email}'`, function (err, result, fields) {
+        if (err) throw err;
+
+        if (result.length) {
+            res.sendFile(__dirname + '/public/signin.html?error=emailTaken')
+        }
+    })
+
     con.query(`INSERT INTO accounts(first_name, last_name, email, password) VALUES ` +
         `('${req.body.firstName}', '${req.body.lastName}', '${req.body.email}', '${encrypt(req.body.password)}')`,
         function (err, result, fields) {
